0df0c38fc8d441a9ed31f5c51e03892f1e61d18c
Minori-Takeuchi <shato.min0716@gmeail.com>
2024-02-17 22:14:20 +0900

    create header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,9 +3,21 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="w-full h-[100px] flex items-center justify-end bg-teal-100 font-comic text-3xl">
-      <nav className="flex justify-between mr-8">
+      <nav className="flex mr-8 space-x-5">
         <Link href="/" className="hover:opacity-50">
           Home
+        </Link>
+        <Link href="/profile" className="hover:opacity-50">
+          Profile
+        </Link>
+        <Link href="#" className="hover:opacity-50">
+          Skills
+        </Link>
+        <Link href="#" className="hover:opacity-50">
+          Works
+        </Link>
+        <Link href="#" className="hover:opacity-50">
+          Contact
         </Link>
       </nav>
     </header>
